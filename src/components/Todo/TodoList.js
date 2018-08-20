88f074ebb948e7d9fd742e9fa152c75d530cdce0
makiintoch <makiintoch@gmail.com>
2018-08-20 14:09:13 +0300

    add TodoList component

---
+++ src/components/Todo/TodoList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="todo-list">
+                todo list
+            </div>
+        );
+    }
+}
+
+export default TodoList;
+
